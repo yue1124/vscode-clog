14ae09a15c77fd2d179e85c16494e26d2b63b8a9
许文悦 <2227215104@qq.com>
2022-04-23 15:00:07 +0800

    basic version, with real-time markdown preview with front matter

---
+++ README.md
@@ -0,0 +1,25 @@
+# Bigyue's Clog Support
+
+this is **A Markdown Preview** with Front Matter for bigyue.cn
+
+In [bigyue.cn](https://bigyue.cn), Clog is a markdown file with front matter (toml). 
+
+In order to see how clog can appear in the bigyue.cn, I write this extension for real-time see the effect.
+
+## Features
+
+### Markdown Preview
+
+when open a `.md` file, you can click `show preview` in the editor toolbar to activate real-time markdown preview.
+
+### Show Front Matter
+
+## Release Notes
+
+### 0.0.1
+
+basic version, with real-time markdown preview with front matter 
+
+## Incoming Features
+
+- frontmatter's modify and autosave ability
---
+++ src/extension.ts
@@ -0,0 +1,147 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as path from 'path';
+
+function getFrontMatterAndContent(text: string) {
+	if (text.startsWith('+++')) {
+		let lineEnd = "";
+		if (text[3] === '\r' && text[4] === '\n') {
+			lineEnd = "\r\n";
+		} else if (text[3] === '\n') {
+			lineEnd = "\n";
+		} else {
+			return ["", text];
+		}
+
+		const pos = text.indexOf(`${lineEnd}+++${lineEnd}`, 3);
+		return [text.substring(3 + lineEnd.length, pos + lineEnd.length),
+		text.substring(pos + 3 + lineEnd.length * 2)];
+	} else {
+		return ["", text];
+	}
+}
+
+function getNonce() {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
+
+function getWebviewHTML(webview: vscode.Webview, extensionPath: string) {
+
+	const nmdJsOnDisk = vscode.Uri.file(path.join(extensionPath, 'statics', 'n-markdown.umd.js'));
+	const nmdJsSrc = webview.asWebviewUri(nmdJsOnDisk);
+
+	const vue3JsOnDisk = vscode.Uri.file(path.join(extensionPath, 'statics', 'vue.global.prod.js'));
+	const vue3JsSrc = webview.asWebviewUri(vue3JsOnDisk);
+
+	const commonmarkJsOnDisk = vscode.Uri.file(path.join(extensionPath, 'statics', 'commonmark.min.js'));
+	const commonmarkJsSrc = webview.asWebviewUri(commonmarkJsOnDisk);
+
+	const nonce = getNonce();
+
+	return `<!DOCTYPE html>
+			<html lang="zh">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src 'unsafe-inline'; script-src 'nonce-${nonce}' 'unsafe-eval';">
+				<script defer nonce="${nonce}" src="${vue3JsSrc}"></script>
+				<script defer nonce="${nonce}" src="${commonmarkJsSrc}"></script>
+				<script defer nonce="${nonce}" src="${nmdJsSrc}"></script>
+				<title>Markdown Preview</title>
+			</head>
+			<body>
+				<div id="app">
+					<n-container>
+						<template #frontmatter>
+							<n-frontmatter :front-matter="frontMatter" />
+						</template>
+						<n-markdown :content="content" style="padding-top= 24px;"/>
+					</n-container>
+				</div>
+				<script nonce="${nonce}" type="module">
+					const vscode = acquireVsCodeApi();
+					const vm = Vue.createApp({
+						data() {
+							return {content: "", frontMatter: ""}
+						}
+					}).use(NMarkdown.default).mount('#app');
+					window.addEventListener('message', event => {
+						vm.frontMatter = event.data.frontMatter;
+						vm.content = event.data.content;
+					})
+				</script>
+			</body>
+			</html>`;
+}
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "clog" is now active!');
+
+	let previewPanel: vscode.WebviewPanel | undefined = undefined;
+	let previewTextEditor: vscode.TextEditor | undefined = undefined;
+	const commandShowPreview = "clog.showPreview";
+
+	const commandShowPreviewHandler = () => {
+
+		const columnToShowIn = (previewPanel && previewPanel.viewColumn) || vscode.ViewColumn.Two;
+		const textDocument = (previewTextEditor && previewTextEditor.document) || vscode.window.activeTextEditor?.document;
+
+		if (textDocument === undefined) {
+			console.log("no activated text document");
+			return;
+		}
+
+		if (previewPanel) {
+			previewPanel.reveal(columnToShowIn);
+		} else {
+			previewPanel = vscode.window.createWebviewPanel(
+				'mdPreview',
+				'Markdown Preview',
+				columnToShowIn,
+				{
+					enableScripts: true,
+					localResourceRoots: [vscode.Uri.file(context.extensionPath)]
+				}
+			);
+
+			previewPanel.webview.html = getWebviewHTML(previewPanel.webview, context.extensionPath);
+
+			const updateContent = (textDocument: vscode.TextDocument, previewPanel: vscode.WebviewPanel) => {
+				if (textDocument.isClosed) {
+					previewPanel.dispose();
+					return;
+				}
+				const [frontMatter, content] = getFrontMatterAndContent(textDocument.getText());
+				previewPanel.webview.postMessage({ frontMatter, content });
+			};
+			updateContent(textDocument, previewPanel);
+			const interval = setInterval(updateContent, 100, textDocument, previewPanel);
+
+			previewPanel.onDidDispose(
+				() => {
+					clearInterval(interval);
+					previewPanel = undefined;
+				},
+				null,
+				context.subscriptions
+			);
+		}
+
+	};
+
+	context.subscriptions.push(vscode.commands.registerCommand(commandShowPreview, commandShowPreviewHandler));
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
